6904875daa685aa77cf23f5d0b748a13bc1f06dd
hageldave <david@RECHENKNECHTLNX>
2013-11-19 20:59:02 +0100

    restructuring and few javadocs

--- src/SuggestionsProvider.java
+++ src/SuggestionsProvider.java
@@ -1,20 +1,53 @@
 import java.awt.Component;
 
-
-public abstract class SuggestionsProvider<E> {
+/**
+ * This abstract class is a framework for a class that makes input based suggestions
+ * and provides them to some {@link Component}. 
+ * <p>
+ * A typical providing cycle starts with an event that calls 
+ * {@link #makeSuggestions(Object)} with the input to process as argument.
+ * The method then creates the suggestions based on the input and calls 
+ * {@link #sendSuggestionsToReceiver(Object[])} which will provide the 
+ * created suggestions to the receiver.
+ * 
+ * @author David Haegele
+ *
+ * @param <S> class that the suggestions are instances of
+ * @param <C> class of the receiver {@link Component}
+ */
+public abstract class SuggestionsProvider<S, C extends Component> {
+	/** Component that receives the suggestions */
+	protected C receiver;
 	
-	Component receiver;
-	
-	public SuggestionsProvider(Component receiver) {
-		setReceiver(receiver);
-	}
-	
-	public void setReceiver(Component receiver){
+	/**
+	 * Constructor that takes the receiving component as argument
+	 * @param receiver {@link Component} that will receive the suggestions
+	 */
+	public SuggestionsProvider(C receiver) {
 		this.receiver = receiver;
 	}
 	
+	/**
+	 * Returns the receiver of this SuggestionsProvider
+	 * @return receiver component
+	 */
+	public C getReceiver(){
+		return receiver;
+	}
+	
+	/**
+	 * Creates suggestions based on the input. <br>
+	 * This method should also make sure that the created suggestions will be
+	 * passed to {@link #sendSuggestionsToReceiver(Object[])}.
+	 * @param input on which the suggestions are based.
+	 */
 	public abstract void makeSuggestions(Object input);
 	
-	public abstract void sendSuggestionsToReceiver(E[] suggestions);
+	/**
+	 * Sends the specified suggestions to the receiver component. <br>
+	 * (or makes the receiver display the suggestions)
+	 * @param suggestions
+	 */
+	public abstract void sendSuggestionsToReceiver(S[] suggestions);
 
 }
--- src/SuggestionsTextField.java
+++ src/SuggestionsTextField.java
@@ -1,7 +1,4 @@
-import java.awt.Component;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
 import javax.swing.JPopupMenu;
@@ -9,72 +6,95 @@ import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+public abstract class SuggestionsTextField extends JTextField {
 
-public class SuggestionsTextField extends JTextField {
-	
-	
 	public static void main(String[] args) {
 		TestFrame f = new TestFrame();
 		f.getContentPane().setLayout(new PhotoCornersLayout());
-		f.getContentPane().add(new SuggestionsTextField(),"topleft(0,0)bottomright(1.0,30)");
-		f.setVisible(true);
-	}
-	
-	public SuggestionsTextField() {
-		final SuggestionsProvider<String> suggester = new SuggestionsProvider<String>(this) {
-			
-			
+		SuggestionsTextField textfield = new SuggestionsTextField() {
+
 			@Override
-			public void sendSuggestionsToReceiver(String[] suggestions) {
-				JPopupMenu popup = new JPopupMenu();
-				for(final String s : suggestions){
-					AbstractAction a = new AbstractAction(s) {
-						
-						@Override
-						public void actionPerformed(ActionEvent arg0) {
-							((JTextField)receiver).setText(s);
-						}
-					};
-					popup.add(a);
-				}
-				popup.show(receiver.getParent(), receiver.getX(), receiver.getY()+receiver.getHeight());
-				receiver.requestFocus();
-			}
-			
-			@Override
-			public void makeSuggestions(Object input) {
-				String s = (String) input;
-				sendSuggestionsToReceiver(s.split(" "));
+			protected void gatherSuggestions(Object input, TextfieldSuggester suggester) {
+				suggester.sendSuggestionsToReceiver(input.toString().split(" "));
 			}
 		};
-		
+		f.getContentPane().add(textfield, "topleft(0,0)bottomright(1.0,30)");
+		f.setVisible(true);
+	}
+
+	public SuggestionsTextField() {
+		final TextfieldSuggester suggester = new TextfieldSuggester(this);
+
 		this.getDocument().addDocumentListener(new DocumentListener() {
-			
+
 			@Override
 			public void removeUpdate(DocumentEvent arg0) {
-				// TODO Auto-generated method stub
 				getSuggestions();
 			}
-			
+
 			@Override
 			public void insertUpdate(DocumentEvent arg0) {
-				// TODO Auto-generated method stub
 				getSuggestions();
 			}
-			
+
 			@Override
 			public void changedUpdate(DocumentEvent arg0) {
 				getSuggestions();
 			}
-			
-			void getSuggestions(){
+
+			void getSuggestions() {
 				suggester.makeSuggestions(getText());
 			}
 		});
-		
-		
+
 	}
 	
+	protected void showSuggestions(String[] suggestions){
+		if (suggestions.length <= 10 && suggestions.length > 0) {
+			JPopupMenu popup = new JPopupMenu();
+			for (final String s : suggestions) {
+				AbstractAction a = new AbstractAction(s) {
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						setText(s);
+					}
+				};
+				popup.add(a);
+			}
+			popup.show(this.getParent(), this.getX(),
+					this.getY() + this.getHeight());
+			this.requestFocus();
+		}
+	}
 	
+	/**
+	 * Gathers suggestions for the specified input and then calls
+	 * {@link TextfieldSuggester#sendSuggestionsToReceiver(String[])}
+	 * of the specified TextfieldSuggester.
+	 * @param input the suggestions are based on.
+	 * @param suggester to send the resulting suggestions.
+	 */
+	protected abstract void gatherSuggestions(Object input, TextfieldSuggester suggester);
+
 	
+	protected static class TextfieldSuggester extends
+			SuggestionsProvider<String, SuggestionsTextField> {
+
+		public TextfieldSuggester(SuggestionsTextField receiver) {
+			super(receiver);
+		}
+
+		@Override
+		public void makeSuggestions(Object input) {
+			getReceiver().gatherSuggestions(input, this);
+		}
+
+		@Override
+		public void sendSuggestionsToReceiver(String[] suggestions) {
+			getReceiver().showSuggestions(suggestions);
+		}
+			
+	}
+
 }

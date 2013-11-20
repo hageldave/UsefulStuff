15b83ce63a705854b22b8ab955e6938d64e99598
hageldave <msv87749@stud.uni-stuttgart.de>
2013-11-20 23:01:40 +0100

    added gnu license comments

--- src/SuggestionsTextField.java
+++ src/SuggestionsTextField.java
@@ -1,5 +1,19 @@
+/*
+ * SuggestionsTextField.java
+ *
+ * Copyright 2013 David Haegele
+ *
+ * This class is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This class is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 import java.awt.event.ActionEvent;
-
 import javax.swing.AbstractAction;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
